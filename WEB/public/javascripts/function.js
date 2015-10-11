89703ac7c9756339377abbff0affb189201b9900
Alan Sanchez <sanchezpineda03@gmail.com>
2015-10-11 04:58:00 -0500

    Connection with API

--- WEB/public/javascripts/function.js
+++ WEB/public/javascripts/function.js
@@ -4,26 +4,69 @@ document.querySelector('head').appendChild(jq);
 
 jq.onload = animations;
 
-function animations()
-{
-	$(document).ready(function() { 
-
-	    $("main").niceScroll();
-
-	  }
-
-	);
-	$document().ready(function() {
-		on(){
-		$('#openPopup').click(function() {
-			$('#popup').show(400, function(){
+function animations() {
+	$(document).ready(function() {
+			$('#open-popup').click(function() {
+				$('#popup').fadeIn('slow');
 			});
-			/* Act on the event */
-		});
-
-		}
-		
+			$('#close-popup').click(function() {
+				$('#popup').fadeOut('slow');
+			});
+			$('#send-place-button').click(function(e) {
+				e.preventDefault();
+				$
+					.post('http://9da38a76.ngrok.io/places', {
+						name: $('#name-id').val(),
+						organization: 2,
+						beaconFactoryID: $('#beacon-id').val()
+					})
+					.done(function () {
+						alert('Listo');
+					});
+			});
+			$('#send-room-button').click(function(e) {
+				e.preventDefault();
+				$
+					.post('http://9da38a76.ngrok.io/rooms', {
+						name: $('#name-id').val(),
+						place: 2,
+						beaconFactoryID: $('#beacon-id').val()
+					})
+					.done(function () {
+						alert('Listo');
+					});
+			});
+			$('#send-event-button').click(function(e) {
+				e.preventDefault();
+				var initial = $('#initial-id').val();
+				var finish = $('#finish-id').val();
+				$
+					.post('http://9da38a76.ngrok.io/events', {
+						name: $('#name-id').val(),
+						place: 2,
+						initialTime: new Date(initial, 12, 0, 0),
+						finishTime: new Date(finish, 12, 30, 0)
+					})
+					.done(function () {
+						alert('Listo');
+					});
+			});
+			$('#send-activity-button').click(function(e) {
+				e.preventDefault();
+				var initial = $('#initial-id').val();
+				var finish = $('#finish-id').val();
+				$
+					.post('http://9da38a76.ngrok.io/activities', {
+						name: $('#name-id').val(),
+						description: $('#description-id').val(),
+						room: 2,
+						event: 2,
+						initialTime: new Date(initial, 12, 0, 0),
+						finishTime: new Date(finish, 12, 30, 0)
+					})
+					.done(function () {
+						alert('Listo');
+					});
+			});
 	});
-
 }
-
--- WEB/routes/index.js
+++ WEB/routes/index.js
@@ -3,9 +3,24 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Beacon Event' });
+  res.render('index', { title: 'Beavent' });
 });
 router.get('/dashboard', function(req, res, next) {
-  res.render('dashboard', { title: 'Dashboard' });
+  res.render('new_place', { title: 'Dashboard - Places' });
+});
+router.get('/dashboard/places', function(req, res, next) {
+  res.render('new_place', { title: 'Dashboard - Places' });
+});
+router.get('/dashboard/rooms', function(req, res, next) {
+  res.render('new_room', { title: 'Dashboard - Rooms' });
+});
+router.get('/dashboard/events', function(req, res, next) {
+  res.render('new_event', { title: 'Dashboard - Events' });
+});
+router.get('/dashboard/activities', function(req, res, next) {
+  res.render('new_activity', { title: 'Dashboard - Activities' });
+});
+router.get('/dashboard', function(req, res, next) {
+  res.render('new_room', { title: 'Dashboard - Rooms' });
 });
 module.exports = router;
